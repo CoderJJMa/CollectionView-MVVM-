969ec5ea62b48cb11ad118bd3c45ae6ce2d73930
CoderJJMa <592357262@qq.com>
2019-04-08 23:46:07 +0800

    数据完善添加图片

--- CollectionViewDemo(MVVM)/others/AppDelegate.m
+++ CollectionViewDemo(MVVM)/others/AppDelegate.m
@@ -17,7 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
+
+    sleep(1);
+
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = [self rootViewController];
     [self.window makeKeyAndVisible];
